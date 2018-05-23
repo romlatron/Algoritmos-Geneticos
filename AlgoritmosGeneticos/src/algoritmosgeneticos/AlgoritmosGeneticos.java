72cdadaaec2176d74f57fc538e1be832a72cb199
ramadis <rramiro.o@hotmail.com>
2018-05-23 13:57:00 -0300

    Fix some issues on main

--- AlgoritmosGeneticos/src/algoritmosgeneticos/AlgoritmosGeneticos.java
+++ AlgoritmosGeneticos/src/algoritmosgeneticos/AlgoritmosGeneticos.java
@@ -5,8 +5,14 @@
  */
 package algoritmosgeneticos;
 
+import crossover.Crossover;
+import crossover.OnePointCrossover;
+import java.util.ArrayList;
+import java.util.List;
+import mutation.Mutation;
+import mutation.UniformMutation;
+import reemplazo.Reemplazo;
 import reemplazo.ReemplazoKMutados;
-import reemplazo.ReemplazoMutarTodos;
 import seleccion.Seleccion;
 import seleccion.SeleccionElite;
 
@@ -24,31 +30,37 @@ public class AlgoritmosGeneticos {
 
         // Logic should look like this. Uncomment when its functional.
 
-        // Reemplazo reemplazar = new ReemplazoKMutados(new SeleccionElite(0)); // 10 is param, 0 is not important since it gets overwritten in replacement.
-        // Seleccion seleccionar = new SeleccionElite((int) (N * gap)); // N is the total number of chromosomes. gap is a parameter between 0 and 1.
-        // chromosomes = new ArrayList();
+        Reemplazo reemplazar = new ReemplazoKMutados(new SeleccionElite(0)); // 10 is param, 0 is not important since it gets overwritten in replacement.
+        Seleccion seleccionar = new SeleccionElite((int) (20)); // N is the total number of chromosomes. gap is a parameter between 0 and 1.
+        Mutation mutar = new UniformMutation(0.2);
+        Crossover recombinar = new OnePointCrossover();
+        List<Chromosome> chromosomes = new ArrayList();
 
-        // Boolean rmt = true;
-        // int x = 20;
-        // // Iterate to stop condition
-        // while (x-- >= 0) {
+        Boolean rmt = true;
+        int x = 20;
+        // Iterate to stop condition
+        while (x-- >= 0) {
 
-        //     // If the replacement strategy is mutate all
-        //     // Do some extra logic.
-        //     if (rmt) {
-        //         seleccionar.setTake(2);
-        //         List<Chromosome> aux = new ArrayList<>();
-        //         List<Chromosome> chroms = chromosomes.clone();
-        //         do {
-        //             aux = reemplazar.apply(mutar.apply(recombinar.apply(seleccionar.apply(chromosomes)), chroms));
-        //             chroms.remove(aux.get(0));
-        //             chroms.remove(aux.get(1));
-        //         } while (aux.size() < chromosomes.size());
-        //         chromosomes = aux;
-        //     } else {
-        //         chromosomes = reemplazar.apply(mutar.apply(recombinar.apply(seleccionar.apply(chromosomes)), chromosomes));
-        //     }
-		// }
+            // If the replacement strategy is mutate all
+            // Do some extra logic.
+            if (rmt) {
+                seleccionar.setTake(2);
+                List<Chromosome> aux = new ArrayList<>();
+                List<Chromosome> chroms = new ArrayList<>(chromosomes);
+                
+                do {
+                    List<Chromosome> temp = reemplazar.apply(mutar.apply(recombinar.apply(seleccionar.apply(chromosomes))), chroms);
+
+                    chroms.remove(temp.get(0));
+                    chroms.remove(temp.get(1));
+                    aux.add(temp.get(0));
+                    aux.add(temp.get(1));
+                } while (aux.size() < chromosomes.size());
+                chromosomes = aux;
+            } else {
+                chromosomes = reemplazar.apply(mutar.apply(recombinar.apply(seleccionar.apply(chromosomes))), chromosomes);
+            }
+        }
     }
     
 }
