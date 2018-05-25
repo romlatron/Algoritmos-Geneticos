a052b66d16d635aedf24de5f409ec4abe3d9e1b0
rlatron <romain.latron@insa-lyon.fr>
2018-05-24 21:07:45 -0300

    All files created

--- AlgoritmosGeneticos/src/algoritmosgeneticos/AlgoritmosGeneticos.java
+++ AlgoritmosGeneticos/src/algoritmosgeneticos/AlgoritmosGeneticos.java
@@ -31,10 +31,11 @@ public class AlgoritmosGeneticos {
         
         // Define methods
         Reemplazo reemplazar = new ReemplazoKMutados(new SeleccionElite(0)); // 10 is param, 0 is not important since it gets overwritten in replacement.
-        Seleccion seleccionar = new SeleccionMixta(new SeleccionRuleta(), new SeleccionElite(), 0.8, 30); // N is the total number of chromosomes. gap is a parameter between 0 and 1.
+        Seleccion seleccionar = new SeleccionMixta(new SeleccionRuleta(), new SeleccionElite(), 0, 30); // N is the total number of chromosomes. gap is a parameter between 0 and 1.
         Mutation mutar = new BitMutation(0.2, 1);
         Crossover recombinar = new OnePointCrossover(0.8);
-        StopCondition condicionCorte = new MaximumGenCondition(10000);
+      //  StopCondition condicionCorte = new StructureCondition(2, 2);
+        StopCondition condicionCorte = new MaximumGenCondition(10);
         Seleccion findBestSelection = new SeleccionElite(1);
 
         System.out.println(findBestSelection.apply(chromosomes));
@@ -59,11 +60,9 @@ public class AlgoritmosGeneticos {
                 chromosomes = aux;
             } else {
                 chromosomes = reemplazar.apply(mutar.apply(recombinar.apply(seleccionar.apply(chromosomes))), chromosomes);
+                System.out.println(findBestSelection.apply(chromosomes));
             }
         }
-        
-        
-        System.out.println(findBestSelection.apply(chromosomes));
     }
     
 }
--- AlgoritmosGeneticos/src/stop_condition/StructureCondition.java
+++ AlgoritmosGeneticos/src/stop_condition/StructureCondition.java
@@ -5,10 +5,45 @@
  */
 package stop_condition;
 
+import algoritmosgeneticos.Chromosome;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Acer
  */
-public class StructureCondition {
+public class StructureCondition implements StopCondition{
     
+    int n; //the number of times the structure does not change in a row
+    int maxN; // the maximum number of n before we stop the algorithm
+    int changement; //the maximum number of same chromosomes to not consider a changement
+    List<Chromosome> previousGeneration;
+            
+    public StructureCondition (int changement, int maxN) {
+        n = 0;
+        this.changement = changement;
+        this.maxN = maxN;
+    }
+    
+    @Override
+    public boolean stop(List<Chromosome> generation) {
+        int notChanged = 0;
+        if (previousGeneration == null) return false;
+        for (Chromosome chromosome : generation) {
+            for (Chromosome prevChromosome : previousGeneration) {
+                if (chromosome.equals(prevChromosome)) {
+                    if (++notChanged > changement) {
+                        if (++n == maxN) return true;
+                        this.previousGeneration = generation;
+                        return false;
+                    }
+                    previousGeneration.remove(prevChromosome);
+                    break;
+                }
+            }
+        }
+        n = 0;
+        return false;
+    }
 }
